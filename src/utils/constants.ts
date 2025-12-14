c4513a0b0c2392f62e2f29328a0b9785b6f00fd7
Fredo <fredotanzil@gmail.com>
2025-12-14 12:33:10 +0700

    done

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // src/utils/constants.ts
 export const rpName = 'Passkey Tutorial';
-export const rpID = 'localhost';
+export const rpID = process.env.RP_ID || 'localhost';
 export const origin = [`http://${rpID}:8080`, `http://127.0.0.1:8080`];
